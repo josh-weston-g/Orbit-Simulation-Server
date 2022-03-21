347987b908666f7ea47bce28fe8db0693293fca6
josh-weston-g <josh.weston.g@gmail.com>
2022-03-21 19:01:29 +0000

    updated databse.py

--- server_side/database.py
+++ server_side/database.py
@@ -1,3 +1,11 @@
 import sqlite3
 
-conn = sqlite3.connect("Orbit-Simulation-Server\planets&moons.db")
+conn = sqlite3.connect("Orbit-Simulation-Server\server_side\planets&moons.db")
+
+c = conn.cursor()
+
+c.execute("SELECT * FROM moons WHERE moonName='Io'")
+print(c.fetchall())
+
+conn.commit()
+conn.close()
